05f869dce41cad44425c4fa6d1ca77b4b3234bed
kamath7 <adithyakamath96@gmail.com>
2023-03-23 23:21:34 +0530

    setup

---
+++ constructor-demo/src/main/java/com/kamathad/constructordemo/Coach.java
@@ -0,0 +1,4 @@
+package com.kamathad.constructordemo;
+
+public interface Coach {
+}
---
+++ constructor-demo/src/test/java/com/kamathad/constructordemo/ConstructorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamathad.constructordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
